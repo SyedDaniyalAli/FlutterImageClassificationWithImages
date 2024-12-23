7fc9bcc07cc2910e8e358c46b790d98dfb307567
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2024-12-23 19:02:50 +0500

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyHomePage());
@@ -19,20 +23,32 @@ class _MyHomePageState extends State<MyHomePage> {
   String result = 'Results will be shown here';
 
   //TODO declare ImageLabeler
+  late ImageLabeler imageLabeler;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     imagePicker = ImagePicker();
-    //TODO initialize labeler
+    // //TODO initialize labeler
+    // final ImageLabelerOptions options =
+    //     ImageLabelerOptions(confidenceThreshold: 0.5);
+    // imageLabeler = ImageLabeler(options: options);
+    loadModel();
+  }
 
+  loadModel() async {
+    final modelPath = await getModelPath('assets/ml/model_mobilenet.tflite');
+    final options = LocalLabelerOptions(
+      confidenceThreshold: 0.5,
+      modelPath: modelPath,
+    );
+    imageLabeler = ImageLabeler(options: options);
   }
 
   @override
   void dispose() {
     super.dispose();
-
   }
 
   //TODO capture image using camera
@@ -48,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
   //TODO choose image using gallery
   _imgFromGallery() async {
     XFile? pickedFile =
-    await imagePicker.pickImage(source: ImageSource.gallery);
+        await imagePicker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
         _image = File(pickedFile.path);
@@ -59,6 +75,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
   //TODO image labeling code here
   doImageLabeling() async {
+    InputImage inputImage = InputImage.fromFile(_image!);
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+
+    result = '';
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+
+      setState(() {
+        result += '$text\n Confidence: ${confidence.toStringAsFixed(2)}\n';
+      });
+    }
+  }
+
+  Future<String> getModelPath(String asset) async {
+    final path = '${(await getApplicationSupportDirectory()).path}/$asset';
+    await Directory(dirname(path)).create(recursive: true);
+    final file = File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(asset);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
   }
 
   @override
@@ -70,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
               image: AssetImage('images/bg.jpg'), fit: BoxFit.cover),
         ),
         child: Scaffold(
-          body:  SingleChildScrollView(
+          body: SingleChildScrollView(
             child: Column(
               children: [
                 const SizedBox(
@@ -91,7 +133,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     Center(
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
+                            backgroundColor: Colors.transparent,
                             shadowColor: Colors.transparent),
                         onPressed: _imgFromGallery,
                         onLongPress: _imgFromCamera,
@@ -99,20 +141,20 @@ class _MyHomePageState extends State<MyHomePage> {
                           margin: const EdgeInsets.only(top: 8),
                           child: _image != null
                               ? Image.file(
-                            _image!,
-                            width: 335,
-                            height: 495,
-                            fit: BoxFit.fill,
-                          )
+                                  _image!,
+                                  width: 335,
+                                  height: 495,
+                                  fit: BoxFit.fill,
+                                )
                               : Container(
-                            width: 340,
-                            height: 330,
-                            child: const Icon(
-                              Icons.camera_alt,
-                              color: Colors.black,
-                              size: 100,
-                            ),
-                          ),
+                                  width: 340,
+                                  height: 330,
+                                  child: const Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.black,
+                                    size: 100,
+                                  ),
+                                ),
                         ),
                       ),
                     ),
